67f0dedb1b3c2514d28e5e2b49d7cfb12e0e9079
Nicole Pingar <nicolepingar@yahoo.com>
2021-09-17 15:00:30 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# work_day_scheduler
+# Work Day Scheduler
+
+## Description
+
+I created this workday scheduler so users can keep track of their meetings & events during the work day. It will conveniently change colors based on the time. Past times are grey, the current time is purple and the future time is pink.  Using JavaScript, this website was built to simplify users' work days. 
+
+## Usage
+
+Below is a picture of the scheduler:
+
+![Picture of Scheduler](assets/images/Capture.PNG)
+
+The link to the scheduler is here: https://nicolepingar.github.io/work_day_scheduler/
+
+## How to Contribute
+Please find me on GitHub @nicolepingar or email me at nicolepingar@gmail.com.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,50 @@
 // Current date at top of page
 $("#currentDay").text(moment().format("dddd, MMM Do, YYYY"));
-
-$(document).ready(function () {
-    function getLocal() {
-        $(".inputField").each(function () {
-            var inputValue = $(this).attr("id");
-            $(this).val(localStorage.getItem(inputValue));   
+// To get data from local storage on refresh
+function getLocal() {
+    $(".inputField").each(function () {
+        var inputValue = $(this).attr("id");
+        $(this).val(localStorage.getItem(inputValue));
+    }
+    )
+}
+// changes colors based on time of day
+function colorChange() {
+    // .inputField sets the location for "this", ".each" allows function to run through each container
+    $(".inputField").each(function () {
+        // "H" format puts time in military hour
+        var todayInTime = moment().format("H")
+        // "this" refers back to .inputfield, each input has a different id#, parseInt turns it into a number 
+        var hour = parseInt($(this).attr("id"))
+        // each class is connected to a different color
+        if (hour < todayInTime) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
         }
-        )  
-    }
-
-    function colorChange() {
-        $(".inputField").each(function () {
-            var todayInTime = moment().format("H")
-            var hour = $(this).attr("id")
-            console.log(hour);
-            console.log(todayInTime);
-            if (hour < todayInTime) {
-                $(this).addClass("past");
-                $(this).removeClass("present");
-                $(this).removeClass("future");
-            }
-            else if (hour == todayInTime) {
-                $(this).addClass("present");
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-            }
-            else {
-                $(this).addClass("future");
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-            }
-        })
-    }
-   
-    $(".saveButton").on("click", function () {
-        var meetings = $(this).siblings(".inputField").val();
-        var time = $(this).siblings().eq(1).attr("id");
-        
-        localStorage.setItem(time, meetings);
-    
+        else if (hour == todayInTime) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
     })
-   getLocal(); 
-   colorChange();
+}
+// "this" now refers to .saveButton
+// when save button is clicked, text from input field is saved in local storage
+$(".saveButton").on("click", function () {
+    // meetings = .saveButton's sibling (inputField)'s, value 
+    var meetings = $(this).siblings(".inputField").val();
+    // time = .saveButton's sibling (inputField)'s id (unique number based on time)
+    var time = $(this).siblings(".inputField").attr("id");
+    // local storage store time as the id # and meetings as the typed input text 
+    localStorage.setItem(time, meetings);
 })
+// calls function to pull data from local storage
+getLocal();
+// calls function to change colors based on time 
+colorChange();
